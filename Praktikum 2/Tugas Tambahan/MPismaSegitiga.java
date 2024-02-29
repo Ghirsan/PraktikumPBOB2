ef5b16a8a67c168793c4f684bfcb45a3eba627f2
Ghirsan <34860660+Ghirsan@users.noreply.github.com>
2024-02-29 18:12:28 +0700

    Add files via upload

---
+++ Praktikum 2/Tugas Tambahan/MPismaSegitiga.java
@@ -0,0 +1,21 @@
+/* 
+ * Nama: Ghirsan Ahdani
+ * NIM: 24060122120019
+ * Main program operasi kelas PrismaSegitiga.
+*/
+
+public class MPismaSegitiga {
+
+    public static void main(String[] args){
+
+        Segitiga tri = new Segitiga(3, 4);
+
+        PrismaSegitiga pristri = new PrismaSegitiga(tri, 5);
+
+        System.out.println("Volume prisma segitiga: " + pristri.hitungVolume());
+
+        System.out.println("Luas permukaan prisma segitiga: " + pristri.hitungLuasPermukaan());
+
+    }
+
+}
---
+++ Praktikum 2/Tugas Tambahan/PrismaSegitiga.java
@@ -0,0 +1,31 @@
+/* 
+ * Nama: Ghirsan Ahdani
+ * NIM: 24060122120019
+ * Kelas PrismaSegitiga dengan atribut alas dan tinggiPrisma serta fungsi operasi objek.
+*/
+
+public class PrismaSegitiga {
+    
+    private Segitiga alas;
+    private double tinggiPrisma;
+
+    public PrismaSegitiga(Segitiga alas, double tinggiPrisma){
+
+        this.alas = alas;
+        this.tinggiPrisma = tinggiPrisma;
+
+    }
+
+    public double hitungVolume(){
+
+        return alas.hitungLuas() * tinggiPrisma;
+
+    }
+
+    public double hitungLuasPermukaan(){
+
+        return (2 * alas.hitungLuas()) + (alas.getAlas() * tinggiPrisma) + (alas.getTinggiSegitiga() * tinggiPrisma) + (Math.sqrt(alas.getAlas() * alas.getAlas() + alas.getTinggiSegitiga() * alas.getTinggiSegitiga()) * tinggiPrisma); // Segitiga siku-siku
+
+    }
+
+}
